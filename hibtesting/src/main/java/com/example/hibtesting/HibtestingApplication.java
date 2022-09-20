9d4b614f5a4160cd710b1d8f43d83ac082cf94d6
Kamil <siepkakamil@gmail.com>
2022-09-20 19:44:34 +0200

    Init repo

---
+++ hibtesting/src/main/java/com/example/hibtesting/HibtestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibtestingApplication.class, args);
+	}
+
+}
---
+++ hibtesting/src/test/java/com/example/hibtesting/HibtestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibtestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
